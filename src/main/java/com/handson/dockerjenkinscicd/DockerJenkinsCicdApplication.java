86a2f83341d5cba616a8ee6e373e7fc3fddf8432
mahesh <mahesh@gmail.com>
2022-11-21 09:16:44 +0530

    initial commit

---
+++ src/main/java/com/handson/dockerjenkinscicd/DockerJenkinsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.handson.dockerjenkinscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/handson/dockerjenkinscicd/DockerJenkinsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handson.dockerjenkinscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
